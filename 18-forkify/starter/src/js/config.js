973d20e0861a048fa4f39609b410f92ec4cefccb
pawar-jayesh <pawar.jayesh.n@gmail.com>
2025-07-17 11:17:25 +0200

    18

---
+++ 18-forkify/starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
+export const TIMEOUT_SEC = 4;
+export const RES_PER_PAGE = 10;
+export const API_KEY = "caf62bce-2ad6-4ab6-9341-63d68171f386";
+export const MODAL_CLOSE_SEC = 2.5;
