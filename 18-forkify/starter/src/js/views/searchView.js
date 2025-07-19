e0bc8fa5f5636147cf7692b575bf971992724e31
pawar-jayesh <pawar.jayesh.n@gmail.com>
2025-07-19 09:16:42 +0200

    18

---
+++ 18-forkify/starter/src/js/views/searchView.js
@@ -0,0 +1,23 @@
+class SearchView{
+    _parentElement = document.querySelector(".search");
+
+    getQuery(){
+        const value = this._parentElement.querySelector(".search__field").value;
+        this._clearInput();
+        return value;
+    }
+
+    _clearInput(){
+        this._parentElement.querySelector(".search__field").value = "";
+    }
+
+    addHandlerSearch(handler){
+        this._parentElement.addEventListener("submit", function(e){
+            e.preventDefault();
+            handler();
+        });
+    }
+
+}
+
+export default new SearchView();
